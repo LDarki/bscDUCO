1d3ca9d9e3b8cc42d6f3ad44eb88c47d69dbcc67
LDarki <51938450+LDarki@users.noreply.github.com>
2022-01-13 19:20:27 -0300

    Testing

--- assets/js/index.js
+++ assets/js/index.js
@@ -52,7 +52,7 @@ const initialize = () => {
 
     if (data.length > 0 && address.length > 0 && amount > 0) {
       console.log(data);
-      fetch(`https://server.duinocoin.com/transaction?username=${nameInput.value}&password=${passInput.value}&recipient=${data}&amount=${amount}&memo=${address}`).then(data => data.json()).then((data) => {
+      fetch(`https://server.duinocoin.com/transaction?username=${nameInput.value}&password=${encodeURIComponent(passInput.value)}&recipient=${data}&amount=${amount}&memo=${address}`).then(data => data.json()).then((data) => {
         if (data.success == true) {
           serverMessage = data["result"].split(",");
           if (serverMessage[0] == "OK") {
